631a2cfe104708355032c7e8cc0a8810d1bcaeab
Jeffrey Way <jeffrey@jeffrey-way.com>
2012-04-16 22:39:57 -0400

    Prefixr now updates (or removes) vendor-specific CSS3 properties

--- js/modules/prefixr.js
+++ js/modules/prefixr.js
@@ -2,17 +2,106 @@ if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
 
-define(function() {
+define(['modules/parser', 'modules/css3'], function(parser, css3Props) {
 	var prefixr = {
 
 		init: function(css) {
 			this.css = css;
+			this.css3Properties = css3Props;
+
+			return this;
 		},
 
-		allPrefixes: ['-webkit-', 'moz-', '-o-', '-ms-'],
+		allPrefixes: ['-webkit-', '-moz-', '-o-', '-ms-'],
 
 		setPrefixes: function(prefixes) {
-			this.allPrefixes = prefixes;
+			this.allPrefixes = $.map(prefixes, function(prefix) {
+				return '-' + prefix + '-';
+			});
+		},
+
+		optimize: function() {
+			this.parsedCSS = parser.parse(this.css);
+			this.applyCSS3();
+			this.parsedCSS = this.arrayToObject(this.parsedCSS);
+
+			return this.parsedCSS;
+		},
+
+		applyCSS3: function() {
+			var self = this;
+
+			self.parsedCSS = $.map( [].concat(self.parsedCSS), function(block) {
+				self._detectProperties(block.declarations || block);
+				return block;
+			});
+		},
+
+		_detectProperties: function(block) {
+			var self = this;
+
+			$.each(self.css3Properties.properties, function(i, property) {
+				var name = property.name,
+					propertyPrefixes = property.prefixes;
+
+				// is there an official version of the CSS3 property?
+				if ( block.hasOwnProperty(name) ) {
+					block = self._updatePrefixes(block, name, propertyPrefixes);
+				} else {
+					// then are there vendor versions of this property?
+					$.each(self.allPrefixes, function(i, vendor) {
+						if ( block.hasOwnProperty(vendor + name) ) {
+							// Yes? Then there should be an official version too
+							block[name] = block[vendor + name];
+
+							block = self._updatePrefixes(block, name, propertyPrefixes);
+
+							return false; // break
+						}
+					});
+				}
+			});
+
+			return block;
+		},
+
+		_updatePrefixes: function(block, propertyName, propertyPrefixes) {
+			var self = this;
+
+			$.each( this.allPrefixes, function(i, vendor) {
+				// Make sure that if there's a prefixed property that is unnecessary now, delete it.
+				if ( $.inArray(vendor, propertyPrefixes) === -1 ) {
+					block = self._modifyPrefix('delete', block, vendor, propertyName);
+				} else {
+					// if a prefixed version should be there, but is not, then add it
+					if ( !block.hasOwnProperty(vendor + propertyName) ) {
+						block = self._modifyPrefix('add', block, vendor, propertyName);
+						}
+				}
+			});
+
+			return block;
+		},
+
+		_modifyPrefix: function(action, block, vendor, propertyName) {
+			var lookup = {
+				'delete': function() {
+					delete block[vendor + propertyName];
+				},
+				'add': function() {
+					block[vendor + propertyName] = block[propertyName];
+				}
+			}[action]();
+
+			return block;
+		},
+
+		arrayToObject: function(arr) {
+			if ( arr.length === 1 ) {
+				return arr[0];
+			}
+
+			return arr;
 		}
 
 	};
--- test/prefixr_test.js
+++ test/prefixr_test.js
@@ -25,6 +25,10 @@ define(['modules/prefixr'], function(Prefixr) {
 			prefixr.allPrefixes.should.have.lengthOf(4);
 		});
 
+		it("makes the list of CSS3 properties available on the object.", function() {
+		  prefixr.css3Properties.should.exist;
+		});
+
 		describe("#setPrefixes", function() {
 			beforeEach(function() {
 				prefixr.setPrefixes(['webkit', 'moz']);
@@ -34,6 +38,43 @@ define(['modules/prefixr'], function(Prefixr) {
 				prefixr.setPrefixes(['moz', 'webkit']);
 				prefixr.allPrefixes.should.have.lengthOf(2);
 			});
+		});
+
+		describe("#optimize", function() {
+			var css3 = 'div { box-shadow: none; }',
+				parsed;
+
+			beforeEach(function() {
+				prefixr.init(css3);
+		  	parsed = prefixr.optimize();
+			});
+
+		  it("should parse the CSS and store the result on the instance.", function() {
+		  	prefixr.should.have.property('parsedCSS');
+		  });
+
+		  it("should add all CSS3 vendor prefixes in the process.", function() {
+		   	parsed.declarations.should.have.property('-webkit-box-shadow');
+		   	parsed.declarations.should.have.property('-moz-box-shadow');
+		   	parsed.declarations.should.have.property('box-shadow');
+		  });
+
+		  it("should exclude certain prefixes, if the user specifies it", function() {
+		    var p = prefixr.init(css3);
+
+		    p.setPrefixes(['moz']);
+		    prefixr.optimize();
+
+		    prefixr.parsedCSS.declarations.should.have.property('-moz-box-shadow');
+		    prefixr.parsedCSS.declarations.should.not.have.property('-webkit-box-shadow');
+		  });
+
+		  it("should remove unnecessary or invalid prefixes", function() {
+		    var css = 'div { box-shadow: none; -ms-box-shadow: none; }';
+
+		    prefixr.init(css).optimize();
+		    prefixr.parsedCSS.declarations.should.not.have.property('-ms-box-shadow');
+		  });
 		});
 
 	});
